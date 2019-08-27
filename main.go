862ce6881a68d7c648b6d44ff3684733f828571d
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-08-27 23:11:52 +0700

    Add: apiv2 support Activities

--- main.go
+++ main.go
@@ -8,9 +8,8 @@ import (
 	"github.com/golang/protobuf/proto"
 )
 
-func main() {
-	// welcome message
-	fmt.Println("Hello Protobuf in Golang")
+// support only name and age
+func apiV1() {
 	// create new protobuf struct
 	person := model.Person{
 		Name: "imkk-000",
@@ -29,6 +28,56 @@ func main() {
 	err = proto.Unmarshal(sendData, &newPerson)
 	fmt.Println("receiveErr:", err)
 	// show receive data name and age
-	fmt.Println("new.person.name:", person.GetName())
-	fmt.Println("new.person.age:", person.GetAge())
+	fmt.Println("new.person.name:", newPerson.GetName())
+	fmt.Println("new.person.age:", newPerson.GetAge())
+}
+
+// support name, age, and activities
+func apiV2() {
+	// create new protobuf struct
+	person := &model.Person{
+		Name: "imkk-000",
+		Age:  111,
+		Activities: []*model.Activity{
+			{
+				Name: "Workout",
+				Time: 60,
+			},
+			{
+				Name: "Sleep",
+				Time: 60 * 8,
+			},
+		},
+	}
+	// try to get name, age, and activities from struct with protobuf function generator
+	fmt.Println("person.name:", person.GetName())
+	fmt.Println("person.age:", person.GetAge())
+	fmt.Println("person.activities:", len(person.GetActivities()))
+	for index, activity := range person.GetActivities() {
+		fmt.Println("person.activities", index, ".name:", activity.GetName())
+		fmt.Println("person.activities", index, ".time:", activity.GetTime())
+	}
+	// convert protobuf struct to binary and prepare for send to another service
+	sendData, err := proto.Marshal(person)
+	fmt.Println("sendData:", sendData)
+	fmt.Println("sendData(string):", strings.TrimSpace(string(sendData)))
+	fmt.Println("sendErr:", err)
+	// convert sendData to receiveData
+	newPerson := model.Person{}
+	err = proto.Unmarshal(sendData, &newPerson)
+	fmt.Println("receiveErr:", err)
+	// show receive data name and age
+	fmt.Println("new.person.name:", newPerson.GetName())
+	fmt.Println("new.person.age:", newPerson.GetAge())
+	fmt.Println("new.person.activities:", len(newPerson.GetActivities()))
+	for index, activity := range newPerson.GetActivities() {
+		fmt.Println("new.person.activities", index, ".name:", activity.GetName())
+		fmt.Println("new.person.activities", index, ".time:", activity.GetTime())
+	}
+}
+
+func main() {
+	// welcome message
+	fmt.Println("Hello Protobuf in Golang")
+	apiV2()
 }
